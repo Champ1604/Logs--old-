42ce08c12288ee47cc7219fef590f26089583371
Champ1604 <puruprathmesh1604@gmail.com>
2017-12-15 00:04:41 +1100

    Update config.js

--- config.js
+++ config.js
@@ -18,7 +18,7 @@ exports.serverid = 'showdown';
 // The nick and password to log in with
 // If no password is required, leave pass empty
 exports.nick = 'Ice Kyubs';
-exports.pass = 'rainYAUtumnleafdance139';
+exports.pass = '';
 
 // The rooms that should be joined.
 // Joining Smogon's Showdown's Lobby is not allowed.
